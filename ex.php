6012f59349c3e107aba7f3b9a4dae8d2f28c1868
Caylin James <caylinjames.dev@gmail.com>
2017-07-21 20:12:07 -0400

    Interfaces vs. Abstract Classes

--- ex.php
+++ ex.php
@@ -1,26 +1,22 @@
 <?php
 
-interface CanBeFiltered {
-    public function filter();
+// Defines a public API - a contract that any implementation HAS to abide by.
+// However, no logic will ever be stored in an interface. (VERY cheap to use!)
+interface Provider {
+    public function getAuthorizeUrl();
 }
 
-class Favorited implements CanBeFiltered {
-    public function filter()
-    {
+// You must 'force' a contract by creating abstract methods.
+// However, this relies heavily on inheritance, which isn't always the best idea.
+abstract class AbstractProvider {
 
-    }
+    abstract protected function getAuthorizationUrl();
 }
 
-class Unwatched implements CanBeFiltered {
-    public function filter()
+class FacebookProvider implements Provider {
+
+    protected function getAuthorizationUrl()
     {
-
-    }
-}
-
-class Difficulty implements CanBeFiltered {
-    public function filter()
-    {
-
+        return '';
     }
 }
